1caa30ad35e498dddf33538bfe7538f66d1fe0a9
ALLANNUNES33 <contato@allannunes.com>
2025-08-23 18:32:23 -0300

    Summary

---
+++ gov.pampersbr.org/verificacao/js/O6ffwWQvxm0N.js
@@ -0,0 +1,24 @@
+(function () {
+<script>
+  window.pixelId = "68795a1240d5b6bd65722f20";
+  var a = document.createElement("script");
+  a.setAttribute("async", "");
+  a.setAttribute("defer", "");
+  a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+  document.head.appendChild(a);
+</script>
+
+
+})();
+
+(function () {
+  const script = document.createElement("script");
+
+  script.src = "https://cdn.utmify.com.br/scripts/utms/latest.js";
+  script.setAttribute("data-utmify-prevent-xcod-sck", "");
+  script.setAttribute("data-utmify-prevent-subids", "");
+  script.async = true;
+  script.defer = true;
+
+  document.head.appendChild(script);
+})();
